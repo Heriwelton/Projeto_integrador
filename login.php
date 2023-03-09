1e4b899a4d012563ca076253100121d4371e6b7f
Heriwelton <126431760+Heriwelton@users.noreply.github.com>
2023-03-08 21:42:36 -0300

    2.0

--- login.php
+++ login.php
@@ -9,7 +9,9 @@
 <html lang="pt-br">
     <head>
         <link rel="stylesheet" href="estilo.css">
-
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+        
         <title>Beauty Key - login</title>
     </head>
 <body>
@@ -18,12 +20,28 @@
             <div class="form-value">
                 <form action="valida_usuario.php" method="post">
                     <h2>login</h2>
-                    <div class="inputbox">
-                        <ion-icon name="mail-outline"></ion-icon>
-                        <input type="text" name="email" required>
-                        <label>Email</label>
+                    
+                    <div class="col-md-12">
+                        <div class="row conteudo-alinhamento justify-content-md-center">
+                            <div id="clicavel" data-target="menu_inicial.php" class="conteudo-servicos align-self-center col-md-4 " >                 	
+                                <h4>Sou cliente</h4>
+                            </div>
+
+                            <div id="clicavel" data-target="menu_inicial.php" class="conteudo-servicos align-self-center col-md-4 ml-md-4"> 
+
+                                <h4>Sou cliente</h4>
+                            </div>        
+                        </div>
                     </div>
                     <div class="inputbox">
+                        
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <input type="text" name="email" required>
+                        <label>CPF</label>
+                        
+                    </div>
+                    <div class="inputbox">
+                        
                         <ion-icon name="lock-closed-outline"></ion-icon>
                         <input type="password" name="senha" required>
                         <label>Senha</label>
@@ -59,7 +77,12 @@
             </div>
         </div>
     </section>
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+
+
+     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
 </body>
 </html>
